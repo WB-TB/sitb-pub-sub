e997db096f4b976d71ec0de845ca5ad1b4a82da3
vinixsatria <vinixsatria@gmail.com>
2026-01-06 18:44:29 +0000

    Buat script SQL untuk bantu instalasi tabel baru dan tambah kolom

---
+++ sql/pubsub_temp.sql
@@ -0,0 +1,22 @@
+-- ckg_pubsub_incoming definition
+
+CREATE TABLE `ckg_pubsub_incoming` (
+  `id` varchar(100) NOT NULL COMMENT 'Message ID from Pub/Sub',
+  `data` TEXT NOT NULL COMMENT 'Message data in JSON format',
+  `received_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Message received timestamp',
+  `processed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Message received timestamp',
+  PRIMARY KEY (`id`),
+  KEY `idx_received_at` (`received_at`),
+  KEY `idx_processed_at` (`processed_at`)
+) ENGINE=InnoDB COMMENT='Pub/Sub Incoming Messages Table';
+
+-- ckg_pubsub_outgoing definition
+
+CREATE TABLE `ckg_pubsub_outgoing` (
+  `id` varchar(100) NOT NULL COMMENT 'Message ID from Pub/Sub',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Record create timestamp',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Record update timestamp',
+  PRIMARY KEY (`id`),
+  KEY `idx_created_at` (`created_at`),
+  KEY `idx_updated_at` (`updated_at`)
+) ENGINE=InnoDB COMMENT='API Outgoing Messages Table';
---
+++ sql/ta_skrining_update.sql
@@ -0,0 +1,2 @@
+-- Add ckg_id column to ta_skrining table
+ALTER TABLE ta_skrining ADD COLUMN ckg_id varchar(16) DEFAULT NULL;
